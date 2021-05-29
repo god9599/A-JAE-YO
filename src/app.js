fafea2b9c8d06f81d761f969166157b3cbcecd46
god9599 <59276134+god9599@users.noreply.github.com>
2021-05-29 17:13:00 +0900

    [Env] 셋업

---
+++ src/app.js
@@ -0,0 +1,48 @@
+import express from "express";
+import path from "path";
+import logger from "morgan";
+import indexRouter from "./routes/index.js";
+
+const __dirname = path.resolve();
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(logger("combined"));
+} else {
+  app.use(logger("dev"));
+}
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  const error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500).json({
+    message: err.message,
+    error,
+  });
+});
+
+app.listen(app.get("port"), () => {
+  console.log(
+    `!!!App is running at http://localhost:${app.get("port")} in ${app.get(
+      "env"
+    )} mode!!!`
+  );
+});
+
+export default app;
