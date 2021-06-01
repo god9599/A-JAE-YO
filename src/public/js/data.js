d20b6fe21d510715e7df093c76292e0d933101aa
god9599 <59276134+god9599@users.noreply.github.com>
2021-06-02 02:45:36 +0900

    [Etc] 데이터 정의

---
+++ src/public/js/data.js
@@ -0,0 +1,145 @@
+const qnaList = [
+  {
+    question:
+      "https://mblogthumb-phinf.pstatic.net/20151102_73/shcjlove1_1446464727323MhbxO_JPEG/067.jpg?type=w2",
+    example: [
+      { answer: "본오본" },
+      {
+        answer: "초코볼",
+      },
+      { answer: "팡스타" },
+    ],
+  },
+  {
+    question:
+      "https://mblogthumb-phinf.pstatic.net/20151231_125/slslvk131_1451560025942nYfk3_JPEG/%B8%F0.jpg?type=w2",
+    example: [
+      { answer: "슈암" },
+      {
+        answer: "파이어룸",
+      },
+      {
+        answer: "싱가",
+      },
+    ],
+  },
+  {
+    question:
+      "https://image.edaily.co.kr/images/photo/files/NP/S/2020/04/PS20042500008.jpg",
+    example: [
+      { answer: "스킬자수" },
+      { answer: "자수뜨기" },
+      {
+        answer: "스킬뜨개",
+      },
+    ],
+  },
+  {
+    question: "https://t1.daumcdn.net/cfile/blog/99EC1A3F5B1AD3C420",
+    example: [
+      { answer: "천재 소년 지미 뉴트론" },
+      {
+        answer: "말썽구러기 휘저",
+      },
+      {
+        answer: "리키의 시간 여행",
+      },
+    ],
+  },
+  {
+    question:
+      "http://image.ohmynews.com/down/images/1/songjh03_196147_1[254454].jpg",
+    example: [
+      { answer: "알코올" },
+      {
+        answer: "불소",
+      },
+      {
+        answer: "손소독제",
+      },
+    ],
+  },
+
+  {
+    question: "https://wooriclass.co.kr/wp-content/uploads/2017/06/3-204.jpg",
+    example: [
+      { answer: "행운의 뽑기 장난감" },
+      {
+        answer: "수동 물품 세척기",
+      },
+      { answer: "지우개 청소기" },
+    ],
+  },
+  {
+    question:
+      "https://dnvefa72aowie.cloudfront.net/origin/article/202101/f5169a5c8e1c2e1bc7376c757bf102446f284fd80ddfcfc7280570feb1d2400b.webp?q=95&s=1440x1440&t=inside",
+    example: [
+      { answer: "호신용 호루라기" },
+      { answer: "레이저 포인터" },
+      {
+        answer: "총알 모양 악세서리",
+      },
+    ],
+  },
+  {
+    question:
+      "http://www.busan.com/nas/data/content/image/2017/06/02/20170602000141_0.jpg",
+    example: [
+      { answer: "이젠 걱정안해두 돼. 내가 책임질게" },
+      {
+        answer: "이거 안놓으면 경찰에 신고할거에요!!",
+      },
+      { answer: "애기야! 가자~" },
+    ],
+  },
+  {
+    question: "https://t1.daumcdn.net/cfile/blog/2149CB4F56C98FDE05",
+    example: [
+      {
+        answer: "구슬 동자",
+      },
+      {
+        answer: "구슬몬",
+      },
+      {
+        answer: "레오",
+      },
+    ],
+  },
+  {
+    question:
+      "http://file.instiz.net/data/cached_img/upload/a/1/8/a180e01a1df7ec3ca63643cd0bee9168.png",
+    example: [
+      {
+        answer: "젤리",
+      },
+      {
+        answer: "초콜릿",
+      },
+      { answer: "사탕" },
+    ],
+  },
+];
+
+const infoList = [
+  {
+    result:
+      "https://i.pinimg.com/originals/2d/96/27/2d962753eacacc8361ee32902a300d58.jpg",
+    desc: "오우오우 당신은 찐 아재 입니다.. 꼰대 소리 듣지 않게 조심하세용!^^ 딱딱!",
+  },
+  {
+    result:
+      "https://littledeep.com/wp-content/uploads/2020/09/employee-illustration-png-ai.png",
+    desc: "아재가 되어가는 과정입니다.. 시간은 빠르니 주의하세용ㅎㅅㅎ",
+  },
+  {
+    result:
+      "https://www.urbanbrush.net/web/wp-content/uploads/edd/2021/03/urbanbrush-20210328201908204092.jpg",
+    desc: "당신은 신세대에 더 가깝군요!! 푸하하 만반잘부!!",
+  },
+  {
+    result:
+      "https://littledeep.com/wp-content/uploads/2019/05/littledeep_baby_style1.png",
+    desc: "응앵응애애앵~~ 어제 태어나셨나요?",
+  },
+];
